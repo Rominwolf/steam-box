2c3b3e3bfe99ac399bac75300978e971ca046434
Rominwolf <im@wsm.ink>
2021-11-12 23:28:39 +0800

    Update main.go

--- cmd/box/main.go
+++ cmd/box/main.go
@@ -67,13 +67,13 @@ func main() {
 	)
 
 	if steamOption == "ALLTIME" {
-		filename = "🎮 Steam playtime leaderboard"
+		filename = "🎮 @Rominwolf's Steam game playtime"
 		lines, err = box.GetPlayTime(ctx, steamID, multiLined, appIDList...)
 		if err != nil {
 			panic("GetPlayTime err:" + err.Error())
 		}
 	} else if steamOption == "RECENT" {
-		filename = "🎮 Recently played Steam games"
+		filename = "🎮 @Rominwolf's recently played Steam games"
 		lines, err = box.GetRecentGames(ctx, steamID, multiLined)
 		if err != nil {
 			panic("GetRecentGames err:" + err.Error())
